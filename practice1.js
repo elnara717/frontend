528efff53ab36bc473e607aa72b4b062a63b3136
Elnara <elnarahank@gmail.com>
2023-05-18 12:59:14 -0400

    JavaScript objects and Conditional statements

---
+++ practice1.js
@@ -0,0 +1,67 @@
+// This is how we comment in one line
+
+/*
+ * This is how we comment in multiple line
+*/
+
+
+
+console.log("Hello World");
+
+console.log('Hello World in single quote');
+
+// 3 ways we can define variable
+// var , let
+// const if the variable value is fixes and can not be changed
+
+/* 
+ * JavaScript is not a strongly typed language
+ * it's a case sensitive language 
+ * 
+*/
+
+let message = "Hello to JavaScript";
+console.log(message);
+// re-assigning value
+message = "JavaScript is fun!" ;
+console.log(message);
+
+var num = 10 ; 
+console.log(num);
+
+let price = 1399.99;
+console.log("Iphone price is "+ price);
+
+let hired = true ;
+console.log("Is Marina Hired? : "+ hired);
+
+let coffee;
+console.log(coffee);
+
+let negativity = null ;
+console.log(negativity);
+
+// creating a variable that can not change
+// const --> short for constant, a value that does not change
+
+const bla = "John";
+// name = "smt else"; // this will throw an error, can not re-assign the const
+console.log(bla);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practice2.js
@@ -0,0 +1,50 @@
+
+let num = 100;
+
+// Arithmetic Operators  + - * / % 
+  // ++ --
+console.log(num+5);
+console.log(num-5);
+console.log(num*5);
+console.log(num/5);
+console.log(num%5);
+
+
+// Assignments Operators
+    // = += -= *= /= %=
+console.log(num+= 100);
+console.log(num-= 50);
+console.log(num*=2);
+console.log(num/=2);
+
+console.log('--------------');
+
+
+
+// Relational|Comparison Operators
+    // == === != !== > >= < <=
+let price = 99;
+let strPrice = '99';
+console.log(price == strPrice); // true: just check for content equality
+console.log(price === strPrice); // false: check for content and equality
+
+console.log(price != 99); // false
+console.log(price !== '99');
+
+console.log('-----------------');
+
+// Logical Operators && || &, |
+  // check if the price  between 50 - 100
+console.log(price > 50 && price < 100); // true
+
+// check the price is less than 10 or greater than 100
+console.log(price < 10 || price >100); // false
+
+
+
+
+
+
+
+
+
